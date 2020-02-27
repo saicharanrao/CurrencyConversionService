ffaafcfd13d1e1cd7465eb79a39cd820a8d355a2
SaiCharan Vennamanani <SAI_C_VENNAMANANI@homedepot.com>
2020-02-27 16:56:28 -0600

    Ribbon load balancing

--- src/main/java/com/charan/learning/CurrencyConversionService/proxy/Proxy.java
+++ src/main/java/com/charan/learning/CurrencyConversionService/proxy/Proxy.java
@@ -1,12 +1,14 @@
 package com.charan.learning.CurrencyConversionService.proxy;
 
 import com.charan.learning.CurrencyConversionService.domain.CurrencyConversionEntity;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 
-@FeignClient(name="forex-service", url="localhost:8000")
+@FeignClient(name="forex-service")
+@RibbonClient(name="forex-service")
 public interface Proxy {
   @GetMapping("/currency-exchange/from/{from}/to/{to}")
   public CurrencyConversionEntity retrieveExchangeValue
